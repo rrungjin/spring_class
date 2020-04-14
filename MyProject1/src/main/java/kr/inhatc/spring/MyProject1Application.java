5a1f8cd461b19ed5d38cf5245b441e053e5c626e
rrungjin <rrungjin@DESKTOP-FIPTTVV.mshome.net>
2020-04-15 05:04:23 +0900

    1 commit

---
+++ MyProject1/src/main/java/kr/inhatc/spring/MyProject1Application.java
@@ -0,0 +1,13 @@
+package kr.inhatc.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyProject1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyProject1Application.class, args);
+	}
+
+}
